226ed71c83763e97e4f5cb8d24f0384a5190835c
CheezItMan <chris@adadevelopersacademy.org>
2020-11-27 16:31:46 -0800

    update calculator.js to abstract out user input

--- calculator.js
+++ calculator.js
@@ -1,5 +1,28 @@
+
 const exampleAdditionInput = {
   num1: 3,
   num2: 5,
   operation: 'add',
 }
+
+const calculateUserInput = function (error, promptInput) {
+  console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+
+  // Questions to ask and answer:
+  // What is promptInput?
+  // What data type? What does it hold? What does it represent?
+  // How do we read values from it? What syntax?
+  // How can we use it?
+  // Can we call our existing functions now, inside of this function?
+}
+
+// Example manual testing of calculator.  
+// calculateUserInput({}, {
+//   num1: 3,
+//   num2: 4,
+//   operation: 'add',
+// });
+
+
+// This exports the function so index.js can import it.
+exports.calculateUserInput = calculateUserInput;
